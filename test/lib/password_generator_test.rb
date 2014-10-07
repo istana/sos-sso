13aac07e1ef2b59f7fa6e217d48fdbcde8733c48
Ivan Stana <stiipa@centrum.sk>
2014-10-08 00:06:45 +0200

    remove echo of password in password generator test

--- test/lib/password_generator_test.rb
+++ test/lib/password_generator_test.rb
@@ -5,7 +5,6 @@ require "password_generator"
 class PasswordGeneratorTest < ActiveSupport::TestCase
 	test 'rememberable password' do
 		pw = PasswordGenerator.rememberable(8)
-		puts pw.inspect
 		assert_equal 8, pw.length
 	end
 end
