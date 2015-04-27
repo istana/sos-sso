93108531719972b27391c4f7a71a4be3ef60370d
Ivan Stana <stiipa@centrum.sk>
2015-04-27 23:55:18 +0200

    Update to Rails 4.2.1, use FFaker instead of Faker, update Paper Trail to 4.0.0 beta to remove deprecation messages. Also configs were updated. Update other gems too.

--- Gemfile
+++ Gemfile
@@ -2,15 +2,15 @@ source 'https://rubygems.org'
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '4.1.6'
+gem 'rails', '4.2.1'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 # Use SCSS for stylesheets
-gem 'sass-rails', '~> 4.0.3'
+gem 'sass-rails', '~> 5.0.3'
 # Use Uglifier as compressor for JavaScript assets
-gem 'uglifier', '>= 1.3.0'
+gem 'uglifier', '>= 2.7.1'
 # Use CoffeeScript for .js.coffee assets and views
-gem 'coffee-rails', '~> 4.0.0'
+#gem 'coffee-rails', '~> 4.0.0'
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
 gem 'therubyracer',  platforms: :ruby
 
@@ -26,7 +26,7 @@ gem 'devise'
 gem 'cancancan'
 
 # history
-gem 'paper_trail'
+gem 'paper_trail', '~> 4.0.0.beta'
 
 # all three above are for rails_admin
 gem 'rails_admin'
@@ -41,9 +41,9 @@ gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
-gem 'jbuilder', '~> 2.0'
+gem 'jbuilder', '~> 2.2.13'
 # bundle exec rake doc:rails generates the API under doc/api.
-gem 'sdoc', '~> 0.4.0',          group: :doc
+gem 'sdoc', '~> 0.4.1',          group: :doc
 
 # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
 gem 'spring',        group: :development
@@ -73,7 +73,7 @@ group :development do
 	gem 'mocha'
 
 	# fake data generator
-	gem 'faker'
+	gem 'ffaker', '~> 2.0'
 
 	# test suite
 	gem 'minitest'
@@ -89,10 +89,10 @@ group :development do
 	gem 'connection_pool'
 	gem 'database_cleaner'
 	
-	gem 'capistrano', '~> 3.2.0', require: false
-	gem 'capistrano-rails', '~> 1.1.1', require: false
+	gem 'capistrano', '~> 3.4.0', require: false
+	gem 'capistrano-rails', '~> 1.1.3', require: false
 	gem 'capistrano-bundler', require: false
-	gem 'capistrano-rbenv', '~> 2.0', require: false
+	gem 'capistrano-rbenv', '~> 2.0.3', require: false
 	gem 'cape', require: false
 	# noecho password to the ssh key
 	gem 'highline', require: false
--- db/schema.rb
+++ db/schema.rb
@@ -13,95 +13,95 @@
 
 ActiveRecord::Schema.define(version: 20140831193453) do
 
-  create_table "admins", force: true do |t|
-    t.string   "email",               default: "", null: false
-    t.string   "encrypted_password",  default: "", null: false
+  create_table "admins", force: :cascade do |t|
+    t.string   "email",               limit: 255, default: "", null: false
+    t.string   "encrypted_password",  limit: 255, default: "", null: false
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",       default: 0,  null: false
+    t.integer  "sign_in_count",       limit: 4,   default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
+    t.string   "current_sign_in_ip",  limit: 255
+    t.string   "last_sign_in_ip",     limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   add_index "admins", ["email"], name: "index_admins_on_email", unique: true, using: :btree
 
-  create_table "aliases", force: true do |t|
-    t.boolean  "active",     default: true, null: false
-    t.string   "name",                      null: false
-    t.integer  "user_id",                   null: false
+  create_table "aliases", force: :cascade do |t|
+    t.boolean  "active",     limit: 1,   default: true, null: false
+    t.string   "name",       limit: 255,                null: false
+    t.integer  "user_id",    limit: 4,                  null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   add_index "aliases", ["active"], name: "index_aliases_on_active", using: :btree
 
-  create_table "cui", id: false, force: true do |t|
-    t.string    "clientipaddress",  default: "", null: false
-    t.string    "callingstationid", default: "", null: false
-    t.string    "username",         default: "", null: false
-    t.string    "cui",              default: "", null: false
-    t.timestamp "creationdate",                  null: false
-    t.datetime  "lastaccounting",                null: false
+  create_table "cui", id: false, force: :cascade do |t|
+    t.string   "clientipaddress",  limit: 255, default: "", null: false
+    t.string   "callingstationid", limit: 255, default: "", null: false
+    t.string   "username",         limit: 255, default: "", null: false
+    t.string   "cui",              limit: 255, default: "", null: false
+    t.datetime "creationdate",                              null: false
+    t.datetime "lastaccounting",                            null: false
   end
 
-  create_table "groups", force: true do |t|
-    t.string   "name",                     null: false
-    t.string   "password",   default: "x", null: false
+  create_table "groups", force: :cascade do |t|
+    t.string   "name",       limit: 255,               null: false
+    t.string   "password",   limit: 255, default: "x", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   add_index "groups", ["name"], name: "unqname", unique: true, using: :btree
 
-  create_table "groups_users", id: false, force: true do |t|
-    t.integer "group_id", null: false
-    t.integer "user_id",  null: false
+  create_table "groups_users", id: false, force: :cascade do |t|
+    t.integer "group_id", limit: 4, null: false
+    t.integer "user_id",  limit: 4, null: false
   end
 
   add_index "groups_users", ["group_id", "user_id"], name: "index_groups_users_on_group_id_and_user_id", using: :btree
 
-  create_table "nas", force: true do |t|
-    t.string  "nasname",                               null: false
-    t.string  "shortname"
-    t.string  "xtype",       default: "other"
-    t.integer "ports"
-    t.string  "secret",      default: "secret",        null: false
-    t.string  "server"
-    t.string  "community"
-    t.string  "description", default: "RADIUS Client"
+  create_table "nas", force: :cascade do |t|
+    t.string  "nasname",     limit: 255,                           null: false
+    t.string  "shortname",   limit: 255
+    t.string  "xtype",       limit: 255, default: "other"
+    t.integer "ports",       limit: 4
+    t.string  "secret",      limit: 255, default: "secret",        null: false
+    t.string  "server",      limit: 255
+    t.string  "community",   limit: 255
+    t.string  "description", limit: 255, default: "RADIUS Client"
   end
 
   add_index "nas", ["nasname"], name: "nasname", using: :btree
 
-  create_table "radacct", primary_key: "radacctid", force: true do |t|
-    t.string   "acctsessionid",                  default: "", null: false
-    t.string   "acctuniqueid",                   default: "", null: false
-    t.string   "username",                       default: "", null: false
-    t.string   "groupname",                      default: "", null: false
-    t.string   "realm",                          default: ""
-    t.string   "nasipaddress",                   default: "", null: false
-    t.string   "nasportid"
-    t.string   "nasporttype"
+  create_table "radacct", primary_key: "radacctid", force: :cascade do |t|
+    t.string   "acctsessionid",        limit: 255, default: "", null: false
+    t.string   "acctuniqueid",         limit: 255, default: "", null: false
+    t.string   "username",             limit: 255, default: "", null: false
+    t.string   "groupname",            limit: 255, default: "", null: false
+    t.string   "realm",                limit: 255, default: ""
+    t.string   "nasipaddress",         limit: 255, default: "", null: false
+    t.string   "nasportid",            limit: 255
+    t.string   "nasporttype",          limit: 255
     t.datetime "acctstarttime"
     t.datetime "acctstoptime"
     t.integer  "acctsessiontime",      limit: 8
-    t.string   "acctauthentic"
-    t.string   "connectioninfo_start"
-    t.string   "connectioninfo_stop"
+    t.string   "acctauthentic",        limit: 255
+    t.string   "connectioninfo_start", limit: 255
+    t.string   "connectioninfo_stop",  limit: 255
     t.integer  "acctinputoctets",      limit: 8
     t.integer  "acctoutputoctets",     limit: 8
-    t.string   "calledstationid",                default: "", null: false
-    t.string   "callingstationid",               default: "", null: false
-    t.string   "acctterminatecause",             default: "", null: false
-    t.string   "servicetype"
-    t.string   "framedprotocol"
-    t.string   "framedipaddress",                default: "", null: false
+    t.string   "calledstationid",      limit: 255, default: "", null: false
+    t.string   "callingstationid",     limit: 255, default: "", null: false
+    t.string   "acctterminatecause",   limit: 255, default: "", null: false
+    t.string   "servicetype",          limit: 255
+    t.string   "framedprotocol",       limit: 255
+    t.string   "framedipaddress",      limit: 255, default: "", null: false
     t.integer  "acctstartdelay",       limit: 8
     t.integer  "acctstopdelay",        limit: 8
-    t.string   "xascendsessionsvrkey"
+    t.string   "xascendsessionsvrkey", limit: 255
   end
 
   add_index "radacct", ["acctsessionid"], name: "acctsessionid", using: :btree
@@ -113,114 +113,114 @@ ActiveRecord::Schema.define(version: 20140831193453) do
   add_index "radacct", ["nasipaddress"], name: "nasipaddress", using: :btree
   add_index "radacct", ["username"], name: "username", using: :btree
 
-  create_table "radcheck", force: true do |t|
-    t.string "username",           default: "",   null: false
-    t.string "attr",               default: "",   null: false
-    t.string "op",       limit: 2, default: "==", null: false
-    t.string "value",              default: "",   null: false
+  create_table "radcheck", force: :cascade do |t|
+    t.string "username", limit: 255, default: "",   null: false
+    t.string "attr",     limit: 255, default: "",   null: false
+    t.string "op",       limit: 2,   default: "==", null: false
+    t.string "value",    limit: 255, default: "",   null: false
   end
 
   add_index "radcheck", ["username"], name: "username", length: {"username"=>32}, using: :btree
 
-  create_table "radgroupcheck", force: true do |t|
-    t.string "groupname",           default: "",   null: false
-    t.string "attr",                default: "",   null: false
-    t.string "op",        limit: 2, default: "==", null: false
-    t.string "value",               default: "",   null: false
+  create_table "radgroupcheck", force: :cascade do |t|
+    t.string "groupname", limit: 255, default: "",   null: false
+    t.string "attr",      limit: 255, default: "",   null: false
+    t.string "op",        limit: 2,   default: "==", null: false
+    t.string "value",     limit: 255, default: "",   null: false
   end
 
   add_index "radgroupcheck", ["groupname"], name: "groupname", length: {"groupname"=>32}, using: :btree
 
-  create_table "radgroupreply", force: true do |t|
-    t.string "groupname",           default: "",   null: false
-    t.string "attr",                default: "",   null: false
-    t.string "op",        limit: 2, default: "==", null: false
-    t.string "value",               default: "",   null: false
+  create_table "radgroupreply", force: :cascade do |t|
+    t.string "groupname", limit: 255, default: "",   null: false
+    t.string "attr",      limit: 255, default: "",   null: false
+    t.string "op",        limit: 2,   default: "==", null: false
+    t.string "value",     limit: 255, default: "",   null: false
   end
 
   add_index "radgroupreply", ["groupname"], name: "groupname", length: {"groupname"=>32}, using: :btree
 
-  create_table "radippool", force: true do |t|
-    t.string   "pool_name",                     null: false
-    t.string   "framedipaddress",  default: "", null: false
-    t.string   "nasipaddress",     default: "", null: false
-    t.string   "calledstationid",               null: false
-    t.string   "callingstationid",              null: false
+  create_table "radippool", force: :cascade do |t|
+    t.string   "pool_name",        limit: 255,              null: false
+    t.string   "framedipaddress",  limit: 255, default: "", null: false
+    t.string   "nasipaddress",     limit: 255, default: "", null: false
+    t.string   "calledstationid",  limit: 255,              null: false
+    t.string   "callingstationid", limit: 255,              null: false
     t.datetime "expiry_time"
-    t.string   "username",         default: "", null: false
-    t.string   "pool_key",                      null: false
+    t.string   "username",         limit: 255, default: "", null: false
+    t.string   "pool_key",         limit: 255,              null: false
   end
 
   add_index "radippool", ["framedipaddress"], name: "framedipaddress", using: :btree
   add_index "radippool", ["nasipaddress", "pool_key", "framedipaddress"], name: "radippool_nasip_poolkey_ipaddress", using: :btree
   add_index "radippool", ["pool_name", "expiry_time"], name: "radippool_poolname_expire", using: :btree
 
-  create_table "radpostauth", force: true do |t|
-    t.string   "username", default: "", null: false
-    t.string   "pass",     default: "", null: false
-    t.string   "reply",    default: "", null: false
-    t.datetime "authdate",              null: false
+  create_table "radpostauth", force: :cascade do |t|
+    t.string   "username", limit: 255, default: "", null: false
+    t.string   "pass",     limit: 255, default: "", null: false
+    t.string   "reply",    limit: 255, default: "", null: false
+    t.datetime "authdate",                          null: false
   end
 
-  create_table "radreply", force: true do |t|
-    t.string "username",           default: "",   null: false
-    t.string "attr",               default: "",   null: false
-    t.string "op",       limit: 2, default: "==", null: false
-    t.string "value",              default: "",   null: false
+  create_table "radreply", force: :cascade do |t|
+    t.string "username", limit: 255, default: "",   null: false
+    t.string "attr",     limit: 255, default: "",   null: false
+    t.string "op",       limit: 2,   default: "==", null: false
+    t.string "value",    limit: 255, default: "",   null: false
   end
 
   add_index "radreply", ["username"], name: "username", length: {"username"=>32}, using: :btree
 
-  create_table "radusergroup", id: false, force: true do |t|
-    t.string  "username",  default: "", null: false
-    t.string  "groupname", default: "", null: false
-    t.integer "priority",  default: 1,  null: false
+  create_table "radusergroup", id: false, force: :cascade do |t|
+    t.string  "username",  limit: 255, default: "", null: false
+    t.string  "groupname", limit: 255, default: "", null: false
+    t.integer "priority",  limit: 4,   default: 1,  null: false
   end
 
   add_index "radusergroup", ["username"], name: "username", length: {"username"=>32}, using: :btree
 
-  create_table "users", force: true do |t|
-    t.string   "username",                                          null: false
-    t.integer  "gid",           limit: 8,                           null: false
-    t.string   "gecos",                   default: "",              null: false
-    t.string   "homedir",                                           null: false
-    t.string   "shell",                   default: "/usr/bin/rssh", null: false
-    t.string   "password",                default: "x",             null: false
-    t.integer  "lstchg",        limit: 8, default: 1,               null: false
-    t.integer  "min",           limit: 8, default: 0,               null: false
-    t.integer  "max",           limit: 8, default: 9999,            null: false
-    t.integer  "warn",          limit: 8, default: 30,              null: false
-    t.integer  "inact",         limit: 8, default: 0,               null: false
-    t.integer  "expire",        limit: 8, default: -1,              null: false
-    t.integer  "flag",          limit: 1, default: 0,               null: false
-    t.integer  "quota_mass",    limit: 8, default: 52428800,        null: false
-    t.integer  "quota_inodes",  limit: 8, default: 15000,           null: false
-    t.boolean  "active",                  default: true,            null: false
+  create_table "users", force: :cascade do |t|
+    t.string   "username",      limit: 255,                           null: false
+    t.integer  "gid",           limit: 8,                             null: false
+    t.string   "gecos",         limit: 255, default: "",              null: false
+    t.string   "homedir",       limit: 255,                           null: false
+    t.string   "shell",         limit: 255, default: "/usr/bin/rssh", null: false
+    t.string   "password",      limit: 255, default: "x",             null: false
+    t.integer  "lstchg",        limit: 8,   default: 1,               null: false
+    t.integer  "min",           limit: 8,   default: 0,               null: false
+    t.integer  "max",           limit: 8,   default: 9999,            null: false
+    t.integer  "warn",          limit: 8,   default: 30,              null: false
+    t.integer  "inact",         limit: 8,   default: 0,               null: false
+    t.integer  "expire",        limit: 8,   default: -1,              null: false
+    t.integer  "flag",          limit: 1,   default: 0,               null: false
+    t.integer  "quota_mass",    limit: 8,   default: 52428800,        null: false
+    t.integer  "quota_inodes",  limit: 8,   default: 15000,           null: false
+    t.boolean  "active",        limit: 1,   default: true,            null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "ntlm_password"
+    t.string   "ntlm_password", limit: 255
   end
 
   add_index "users", ["active"], name: "index_users_on_active", using: :btree
   add_index "users", ["username"], name: "unqusername", unique: true, using: :btree
 
-  create_table "versions", force: true do |t|
-    t.string   "item_type",      null: false
-    t.integer  "item_id",        null: false
-    t.string   "event",          null: false
-    t.string   "whodunnit"
-    t.text     "object"
+  create_table "versions", force: :cascade do |t|
+    t.string   "item_type",      limit: 255,   null: false
+    t.integer  "item_id",        limit: 4,     null: false
+    t.string   "event",          limit: 255,   null: false
+    t.string   "whodunnit",      limit: 255
+    t.text     "object",         limit: 65535
     t.datetime "created_at"
-    t.text     "object_changes"
+    t.text     "object_changes", limit: 65535
   end
 
   add_index "versions", ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id", using: :btree
 
-  create_table "wimax", force: true do |t|
-    t.string   "username",           default: "", null: false
-    t.datetime "authdate",                        null: false
-    t.string   "spi",                default: "", null: false
-    t.string   "mipkey",             default: "", null: false
+  create_table "wimax", force: :cascade do |t|
+    t.string   "username", limit: 255, default: "", null: false
+    t.datetime "authdate",                          null: false
+    t.string   "spi",      limit: 255, default: "", null: false
+    t.string   "mipkey",   limit: 255, default: "", null: false
     t.integer  "lifetime", limit: 8
   end
 
