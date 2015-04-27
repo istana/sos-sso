9e0fc1abe34eec1f962abf8816b38d63ca8a55e9
Ivan Stana <stiipa@centrum.sk>
2015-04-28 00:05:18 +0200

    add associations version to paper_trail

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140831193453) do
+ActiveRecord::Schema.define(version: 20150427220148) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",               limit: 255, default: "", null: false
@@ -204,6 +204,15 @@ ActiveRecord::Schema.define(version: 20140831193453) do
   add_index "users", ["active"], name: "index_users_on_active", using: :btree
   add_index "users", ["username"], name: "unqusername", unique: true, using: :btree
 
+  create_table "version_associations", force: :cascade do |t|
+    t.integer "version_id",       limit: 4
+    t.string  "foreign_key_name", limit: 255, null: false
+    t.integer "foreign_key_id",   limit: 4
+  end
+
+  add_index "version_associations", ["foreign_key_name", "foreign_key_id"], name: "index_version_associations_on_foreign_key", using: :btree
+  add_index "version_associations", ["version_id"], name: "index_version_associations_on_version_id", using: :btree
+
   create_table "versions", force: :cascade do |t|
     t.string   "item_type",      limit: 255,   null: false
     t.integer  "item_id",        limit: 4,     null: false
@@ -212,9 +221,11 @@ ActiveRecord::Schema.define(version: 20140831193453) do
     t.text     "object",         limit: 65535
     t.datetime "created_at"
     t.text     "object_changes", limit: 65535
+    t.integer  "transaction_id", limit: 4
   end
 
   add_index "versions", ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id", using: :btree
+  add_index "versions", ["transaction_id"], name: "index_versions_on_transaction_id", using: :btree
 
   create_table "wimax", force: :cascade do |t|
     t.string   "username", limit: 255, default: "", null: false
