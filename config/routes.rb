165098f6da170cc75595a4d1bf7c15537cae1b3c
Ivan Stana <^_^@myrtana.sk>
2023-06-21 02:28:27 +0200

    remove spring, webpacker configs, reformat routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 	devise_for :admins
-  mount RailsAdmin::Engine => '/', as: 'rails_admin'
+	mount RailsAdmin::Engine => '/', as: 'rails_admin'
 
 	root to: "rails_admin/main#dashboard"
 end
